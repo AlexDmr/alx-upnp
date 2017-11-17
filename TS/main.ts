b67819bd8f1686847f19054bac8071c5c84bd367
AlexD <alxdmr2@yahoo.fr>
2017-11-17 19:34:47 +0100

    export types

--- TS/main.ts
+++ TS/main.ts
@@ -1,8 +1,15 @@
 import {getNetInterfacesIPv4} from "./netInterfaces";
 import {ControlPoint} from "./control-point";
+import { Service, StateVariable, CALL } from './Service';
+import {Device} from "./Device";
 
 const CP = new ControlPoint();
 
 export function getControlPoint(): ControlPoint {
     return CP;
 }
+
+export type Service = Service;
+export type Device = Device;
+export type StateVariable = StateVariable;
+export type CALL = CALL;
--- build/main.d.ts
+++ build/main.d.ts
@@ -1,2 +1,8 @@
 import { ControlPoint } from "./control-point";
+import { Service, StateVariable, CALL } from './Service';
+import { Device } from "./Device";
 export declare function getControlPoint(): ControlPoint;
+export declare type Service = Service;
+export declare type Device = Device;
+export declare type StateVariable = StateVariable;
+export declare type CALL = CALL;
